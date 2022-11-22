394011c337ed36ec0fd2224c55afcc373b687f39
JeongHyein <jhi2359@naver.com>
2022-11-22 13:54:18 +0900

    payment 페이지 수정 (서비스 준비중)

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -13,9 +13,32 @@ function Payment() {
             fontSize: "50px",
           }}
         >
-          결제페이지
+          [ 결제페이지 ]
         </a>
       </h1>
+      <h2>
+      <a
+          style={{
+            color: "black",
+            fontSize: "min(6vw, 40px)",
+            fontFamily: "Neo",
+            fontSize: "30px",
+          }}
+        >
+        서비스 준비중....💨
+        </a>
+      </h2>
+
+      <a
+          style={{
+            color: "black",
+            fontSize: "min(6vw, 40px)",
+            fontFamily: "Neo",
+            fontSize: "20px",
+          }}
+        >
+        조금만 기다려주세요 🙌
+        </a>
     </PageDiv>
   );
 }
